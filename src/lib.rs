c33a2ad591330f8c878751822e1149b129661e09
abstract829 <maxxii.1820@gmail.com>
2024-05-20 12:34:45 -0400

    listen to events

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,51 @@
-use zephyr_sdk::{prelude::*, soroban_sdk::xdr::{ScString, ScVal}, EnvClient, DatabaseDerive};
+use zephyr_sdk::{prelude::*, soroban_sdk::{xdr::{ScVal, ContractEvent, Hash, ContractEventBody}, Symbol}, EnvClient, DatabaseDerive};
+
 
 #[derive(DatabaseDerive, Clone)]
 #[with_name("test")]
 struct TestTable {
-    message: ScVal,
+    hello: ScVal,
 }
+
+#[test]
+fn test() {
+    println!("{:?}", stellar_strkey::Contract::from_string("CB74KXQXEGKGPU5C5FI22X64AGQ63NANVLRZBS22SSCMLJDXNHED72MO").unwrap().0);
+}
+
+pub(crate) const ROUTER_CONTRACT_ADDRESS: [u8; 32] = [127, 197, 94, 23, 33, 148, 103, 211, 162, 233, 81, 173, 95, 220, 1, 161, 237, 180, 13, 170, 227, 144, 203, 90, 148, 132, 197, 164, 119, 105, 200, 63];
+
 
 #[no_mangle]
 pub extern "C" fn on_close() {
     let env = EnvClient::new();
 
-    let message = {
-        let message = format!("Hello Soroswap");
-        ScVal::String(ScString(message.try_into().unwrap()))
-    };
-
-    let table = TestTable {
-        message: message.clone(),
-    };
+    let contract_events: Vec<ContractEvent> = env
+    .reader()
+    .soroban_events()
+    .into_iter()
+    .filter(|event| event.contract_id == Some(Hash(ROUTER_CONTRACT_ADDRESS)))
+    .collect();
 
     env.log().debug(
-        "Writing to the database",
-        Some(bincode::serialize(&message).unwrap()),
+        format!(
+            "Processing ledger {} events, total: {}",
+            env.reader().ledger_sequence(), contract_events.len()
+        ),
+        None,
     );
-    table.put(&env);
-    env.log().debug("Successfully wrote to the database", None);
+
+    for event in contract_events {
+        let ContractEventBody::V0(event) = &event.body;
+
+        env.log().debug(
+            format!(
+                "Event with data {:?} and topics {:?}",
+               event.data, event.topics
+            ),
+            None,
+        );
+
+
+    }
+
 }
