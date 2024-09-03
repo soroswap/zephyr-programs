2e4cb5f0740a875752fae36448cca4138db2a2a7
esteblock <estebaniglesiasmanriquez@gmail.com>
2024-09-03 15:04:21 -0400

    add deploy all script

---
+++ deploy_all.sh
@@ -0,0 +1,8 @@
+bash deploy.sh soroswap mainnet
+bash deploy.sh soroswap testnet
+
+bash deploy.sh phoenix mainnet
+bash deploy.sh phoenix testnet
+
+bash deploy.sh aqua mainnet
+bash deploy.sh aqua testnet
