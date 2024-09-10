d32321dde709b15143a1ee8e83eb44b6e7328fa4
esteblock <estebaniglesiasmanriquez@gmail.com>
2024-09-10 16:43:35 -0300

    test rsv

---
+++ scripts/utils/get-soroswap-rsv.ts
@@ -0,0 +1,62 @@
+import * as StellarSdk from "@stellar/stellar-sdk";
+import { getMercuryInstance } from "./mercury";
+
+export interface MercuryRsvCh {
+  address: string;
+  reserveA: string;
+  reserveB: string;
+  timestamp: string; 
+}
+
+export const parseScvalValue = (value: any) => {
+  const scval = StellarSdk.xdr.ScVal.fromXDR(value, "base64");
+  return StellarSdk.scValToNative(scval);
+};
+
+export const parseMercuryScvalResponse = (data: any) => {
+  return data?.map((d: any) => {
+    let n: any = {};
+
+    for (let key in d) {
+      const value = parseScvalValue(d[key]);
+
+      if (typeof value === "bigint" || typeof value === "number") {
+        n[key] = value.toString();
+      } else {
+        n[key] = value;
+      }
+    }
+
+    return n;
+  });
+};
+
+
+export const getSoroswapRsvCh = async (tableName: string, network: "MAINNET" | "TESTNET") => {
+
+  const mercuryInstance = getMercuryInstance(network);  
+  const res = await mercuryInstance.getCustomQuery({
+    request: `query Query {
+    events: ${tableName} {
+      data: nodes {
+        address
+        reserveA
+        reserveB
+        timestamp
+      }
+    }
+  }
+`,
+  });
+
+  
+  if (res.ok) {
+    const parsedData: MercuryRsvCh[] = parseMercuryScvalResponse(
+      res.data?.events?.data
+    );  
+    return parsedData;
+
+  }
+
+  return [];
+};
