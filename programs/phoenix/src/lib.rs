e54a232f93763a935b3981f75cec8aa82835a524
esteblock <estebaniglesiasmanriquez@gmail.com>
2024-09-18 19:19:35 -0300

    make log shorter

--- programs/phoenix/src/lib.rs
+++ programs/phoenix/src/lib.rs
@@ -64,7 +64,7 @@ const CONFIG: Symbol = symbol_short!("CONFIG");
 pub extern "C" fn on_close() {
     let env = EnvClient::new();
     let contract_address_str: &'static str = env!("PHOENIX_FACTORY");
-    env.log().debug(format!("Executing Zephyr Program. Indexing Phoenix Factory Contract: {:?}", &contract_address_str), None);
+    env.log().debug(format!("Indexing PHO Factory: {:?}", &contract_address_str), None);
 
     let factory_contract = stellar_strkey::Contract::from_string(&contract_address_str).unwrap().0;
 
