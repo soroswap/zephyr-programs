66cf11bfe3a2470a5ede8002ac63e1283130f126
hardworking-toptal-dev <161673729+hardworking-toptal-dev@users.noreply.github.com>
2024-09-19 11:53:11 -0400

    fix: remove unnecessary files

--- server/routes/pairs.ts
+++ server/routes/pairs.ts
@@ -1,24 +1,56 @@
 import { Router, Request, Response } from 'express';
 import { ProtocolType } from '../types/protocol';
-import { handlePoolRequest } from "../utils/info/pools/handlePoolRequest";
-import { getMercuryPools, getMercuryPhoenixPools, getMercuryAquaPools } from "../zephyr/helpers";
+import { Network } from '../types/network';
+import { getZephyrTable } from '../../scripts/utils/get-table';
+import { zephyrTableToGraphQLParser } from 'mercury-sdk';
+import { getPairs } from '../../scripts/utils/get-pairs';
 
 const router = Router();
 
-router.get('/', (req: Request, res: Response) => {
+router.get('/', async (req: Request, res: Response) => {
     const queryParams = req.query;
 
     let protocol = queryParams?.protocol as string;
     protocol = protocol?.toUpperCase() as ProtocolType;
 
+    let network = queryParams?.network as string;
+    network = network?.toUpperCase() as Network;
+
+    if (protocol !== "SOROSWAP" && protocol !== "PHOENIX" && protocol !== "AQUA") {
+        return res.status(400).json({ error: "Invalid protocol" });
+    }
+
+    if (network !== "MAINNET" && network !== "TESTNET") {
+        return res.status(400).json({ error: "Invalid network" });
+    }
+
     if (protocol === 'SOROSWAP') {
-        return handlePoolRequest(req, res, getMercuryPools);
+        try {
+            let soroswapPairsTable = getZephyrTable('soroswap_pairs', network)
+            const zephyrTableGraphQL = zephyrTableToGraphQLParser(soroswapPairsTable);
+            const pairs = await getPairs(zephyrTableGraphQL.address, network);
+            return res.json(pairs);
+        } catch (error) {
+            return res.status(500).json({ error: "Failed to fetch pairs" });
+        }
     } else if (protocol === 'PHOENIX') {
-        return handlePoolRequest(req, res, getMercuryPhoenixPools);
+        try {
+            let phoenixPairsTable = getZephyrTable('phoenix_pairs', network)
+            const zephyrTableGraphQL = zephyrTableToGraphQLParser(phoenixPairsTable);
+            const pairs = await getPairs(zephyrTableGraphQL.address, network);
+            return res.json(pairs);
+        } catch (error) {
+            return res.status(500).json({ error: "Failed to fetch pairs" });
+        }
     } else if (protocol === 'AQUA') {
-        return handlePoolRequest(req, res, getMercuryAquaPools);
-    } else {
-        return res.status(400).json({ error: 'Invalid protocol' });
+        try {
+            let aquaPairsTable = getZephyrTable('aqua_pairs', network)
+            const zephyrTableGraphQL = zephyrTableToGraphQLParser(aquaPairsTable);
+            const pairs = await getPairs(zephyrTableGraphQL.address, network);
+            return res.json(pairs);
+        } catch (error) {
+            return res.status(500).json({ error: "Failed to fetch pairs" });
+        }
     }
 }
 );
