3f77fb5333a4048e4251d218bf5574d8f476041f
mauroepce <mauricioeslainerpc@gmail.com>
2024-11-29 13:14:27 -0300

    removing jest for the moment

--- server/tests/tokens.test.ts
+++ server/tests/tokens.test.ts
@@ -1,18 +1,18 @@
-import request from 'supertest';
-import app from '../src/app';
+// import request from 'supertest';
+// import app from '../src/app';
 
-describe('GET /api/tokens', () => {
-  it('should return a list of tokens', async () => {
-    const response = await request(app)
-      .get('/api/tokens')
-      .query({ network: 'TESTNET' });
+// describe('GET /api/tokens', () => {
+//   it('should return a list of tokens', async () => {
+//     const response = await request(app)
+//       .get('/api/tokens')
+//       .query({ network: 'TESTNET' });
 
-    expect(response.status).toBe(200);
-    expect(Array.isArray(response.body)).toBeTruthy();
-  });
+//     expect(response.status).toBe(200);
+//     expect(Array.isArray(response.body)).toBeTruthy();
+//   });
 
-  it('should return error 400 if parameters are missing', async () => {
-    const response = await request(app).get('/api/tokens');
-    expect(response.status).toBe(400);
-  });
-});
+//   it('should return error 400 if parameters are missing', async () => {
+//     const response = await request(app).get('/api/tokens');
+//     expect(response.status).toBe(400);
+//   });
+// });
