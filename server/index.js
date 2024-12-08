116b1ea0609daf706875e44794dd7c21228d40be
Nicolás Poblete <nipobletem@gmail.com>
2024-12-08 01:16:51 -0300

    Add routes handling

--- server/index.js
+++ server/index.js
@@ -5,10 +5,14 @@ const routes = require('./routes');
 const app = express()
 const PORT = 4000
 
-app.use('/', routes);
+app.use('/api', routes);
 
 app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `)
+})
+
+app.get('/', (req, res) => {
+  res.send('XLM Prices')
 })
 
 // Export the Express API
