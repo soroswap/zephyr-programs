2db51be4d077aaba0a984c884f0c1120bcc8d8cf
Nicolás Poblete <nipobletem@gmail.com>
2024-12-11 15:20:47 -0300

    From js to ts

---
+++ server/pages/api/swagger.ts
@@ -0,0 +1,35 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+const swaggerJSDoc = require('swagger-jsdoc')
+
+const swaggerDefinition = {
+    openapi: '3.0.0',
+    info: {
+        title: 'API Documentation',
+        version: '1.0.0',
+        description: 'This is the API documentation for my Next.js application',
+    },
+    servers: [
+        {
+          url: 'https://zephyr-programs-blush.vercel.app',
+        },
+    ],
+}
+
+const options = {
+    swaggerDefinition,
+    apis: ['./pages/api/**/*.{ts,tsx}'],
+}
+
+const swaggerSpec = swaggerJSDoc(options)
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    try{
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(swaggerSpec)
+    } catch (error) {
+        console.error('Swagger spec error:', error);
+        res.status(500).json({ error: 'Unable to generate Swagger spec' });
+    }
+}
