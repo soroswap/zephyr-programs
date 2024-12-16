fb6d9c6f05ccd2ed47fc8de86bccd6e66bda948d
Nicolás Poblete <nipobletem@gmail.com>
2024-12-15 23:33:05 -0300

    jwt script helper deprecation

--- programs/soroswap/mercury-helpers/src/scripts/jwt.ts
+++ programs/soroswap/mercury-helpers/src/scripts/jwt.ts
@@ -1,17 +1,17 @@
-import { mercuryInstance } from "../utils/mercury.js";
+// import { getMercuryInstance } from "../../../../../src/zephyr/mercury.js";
 
-(async () => {
-  const res = await mercuryInstance.updateAccessToken();
+// (async () => {
+//   const res = await getMercuryInstance.updateAccessToken();
 
-  const token = res.data?.authenticate.jwtToken;
+//   const token = res.data?.authenticate.jwtToken;
 
-  console.log("TOKEN");
-  console.log("-".repeat(50));
-  console.log(token);
-  console.log("-".repeat(50));
+//   console.log("TOKEN");
+//   console.log("-".repeat(50));
+//   console.log(token);
+//   console.log("-".repeat(50));
 
-  console.log("GRAPHIQL HEADER");
-  console.log("-".repeat(50));
-  console.log(`{"Authorization": "Bearer ${token}"}`);
-  console.log("-".repeat(50));
-})();
+//   console.log("GRAPHIQL HEADER");
+//   console.log("-".repeat(50));
+//   console.log(`{"Authorization": "Bearer ${token}"}`);
+//   console.log("-".repeat(50));
+// })();
